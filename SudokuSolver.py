c79b39274324320b747f508bcbfc5c37d7c81882
vicious <vicious987@gmail.com>
2021-10-19 13:09:35 +0200

    turn Sudoku static class into module

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import SudokuToolset
 
 t1 = [
 #    0 1 2  3 4 5  6 7 8 
@@ -17,69 +18,7 @@ t1 = [
 
 t = t1 
 
-class SudokuToolset:
-    BLANK = 0
-    DIGITS = set(range(1,10))
-
-    @staticmethod
-    def get_row(board, row):
-        return set(board[row])
-
-    @staticmethod
-    def get_col(board, col):
-        return set([board[row][col] for row in range(9)])
-
-    @staticmethod
-    def get_box(board, row, col):
-        i, j= row // 3, col // 3
-        sqr = [row[j*3:j*3+3] for row in board[i*3:i*3+3]] #sqr as list of rows
-        return set([x for sub in sqr for x in sub]) #flattening
-
-    @staticmethod
-    def get_viable_vertical(board, row) -> set:
-        return SudokuToolset.DIGITS - SudokuToolset.get_row(board, row)
-
-    @staticmethod
-    def get_viable_horizontal(board, col) -> set:
-        return SudokuToolset.DIGITS - SudokuToolset.get_col(board, col)
-
-    @staticmethod
-    def get_viable_box(board, row, col) -> set:
-        return SudokuToolset.DIGITS - SudokuToolset.get_box(board, row, col)
-    @staticmethod
-    def get_viable_digits(board, row, col) -> set:
-        return SudokuToolset.get_viable_vertical(board, row) & SudokuToolset.get_viable_horizontal(board, col) & SudokuToolset.get_viable_box(board, row, col)
-
-    @staticmethod
-    def find_blank(board):
-        for i in range(9):
-            for j in range(9):
-                if board[i][j] == SudokuToolset.BLANK:
-                    return i, j
-        return None, None
-                #return (i, j) if board[i][j] == SudokuToolset.BLANK else (None, None)
-
-    @staticmethod
-    def fill(board):
-        x, y = SudokuToolset.find_blank(board)
-        if x is None:
-            return True
-        
-        for d in SudokuToolset.DIGITS:
-            if d in SudokuToolset.get_viable_digits(board, x, y):
-                board[x][y] = d
-                if SudokuToolset.fill(board):
-                    return True
-                board[x][y] = SudokuToolset.BLANK
-        return False
-
-    @staticmethod
-    def printboard(board):
-        for row in board:
-            for i in row:
-                print(i, end =" ")
-            print()
-
+#tools.printboard(t)
 SudokuToolset.printboard(t)
 SudokuToolset.fill(t)
 print("---------------------------------------")
---
+++ SudokuToolset.py
@@ -0,0 +1,52 @@
+BLANK = 0
+DIGITS = set(range(1,10))
+
+def get_row(board, row):
+    return set(board[row])
+
+def get_col(board, col):
+    return set([board[row][col] for row in range(9)])
+
+def get_box(board, row, col):
+    i, j= row // 3, col // 3
+    sqr = [row[j*3:j*3+3] for row in board[i*3:i*3+3]] #sqr as list of rows
+    return set([x for sub in sqr for x in sub]) #flattening
+
+def get_viable_vertical(board, row) -> set:
+    return DIGITS - get_row(board, row)
+
+def get_viable_horizontal(board, col) -> set:
+    return DIGITS - get_col(board, col)
+
+def get_viable_box(board, row, col) -> set:
+    return DIGITS - get_box(board, row, col)
+
+def get_viable_digits(board, row, col) -> set:
+    return get_viable_vertical(board, row) & get_viable_horizontal(board, col) & get_viable_box(board, row, col)
+
+def find_blank(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == BLANK:
+                return i, j
+    return None, None
+            #return (i, j) if board[i][j] == BLANK else (None, None)
+
+def fill(board):
+    x, y = find_blank(board)
+    if x is None:
+        return True
+    
+    for d in DIGITS:
+        if d in get_viable_digits(board, x, y):
+            board[x][y] = d
+            if fill(board):
+                return True
+            board[x][y] = BLANK
+    return False
+
+def printboard(board):
+    for row in board:
+        for i in row:
+            print(i, end =" ")
+        print()
