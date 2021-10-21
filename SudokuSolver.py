8bb8dfe6ff353f45584a5c35793d70588393950e
vicious <vicious987@gmail.com>
2021-10-21 12:39:10 +0200

    create config file

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import SudokuToolset as SudokuTools
+import config as cfg
 import pygame
-from collections import Counter
 
 t1 = [
 #    0 1 2  3 4 5  6 7 8 
@@ -40,21 +40,13 @@ t = t1
 #interupt gui_solver
 #make config files
 
-BG_COLOR = WHITE = (255, 255, 255)
-FOCUS_COLOR = RED = (255,0,0)
-GREEN = (0,255,0)
-GAME_DIGIT = BLACK = (0,0,0)
-PLAYER_DIGIT = GRAY = (150,150,150)
-
-DELAY = 0
-
 class SudokuGrid():
 
     def __init__(self, surf, matrix):
         self.surface = surf
         self.matrix = matrix
         self.width, self.height = surf.get_width() , surf.get_height()
-        self.squares = [[Box(self.surface, i, j, matrix[i][j]) for j in range(9)] for i in range(9)]
+        self.squares = [[Square(self.surface, i, j, matrix[i][j]) for j in range(9)] for i in range(9)]
         self.sqr_size = self.width / 9
         self.selected_sqr = None
 
@@ -62,8 +54,8 @@ class SudokuGrid():
         for i in range(1,10):
             thickness = 1 if i % 3 != 0 else 4
             j = i * self.sqr_size
-            pygame.draw.line(self.surface, BLACK, (0, j), (self.width, j), thickness)
-            pygame.draw.line(self.surface, BLACK, (j, 0), (j, self.height), thickness)
+            pygame.draw.line(self.surface, cfg.LINE_COLOR, (0, j), (self.width, j), thickness)
+            pygame.draw.line(self.surface, cfg.LINE_COLOR, (j, 0), (j, self.height), thickness)
 
         for row in self.squares:
             for sqr in row:
@@ -72,7 +64,7 @@ class SudokuGrid():
     def deselect(self):
         if self.selected_sqr is None:
             return
-        self.selected_sqr.focus = False
+        self.selected_sqr.highlighted = False
         self.selected_sqr = None
     
     def set_both(self, r, c, d):
@@ -96,14 +88,14 @@ class SudokuGrid():
                 self.set_both(r, c, d)
                 self.squares[r][c].solver_draw(backtracked = False)
                 pygame.display.update()
-                pygame.time.delay(DELAY)
+                pygame.time.delay(cfg.SOLVER_DELAY)
                 if self.solve_and_draw():
                     return True
 
                 self.set_both(r, c, 0)
                 self.squares[r][c].solver_draw(backtracked = True)
                 pygame.display.update()
-                pygame.time.delay(DELAY)
+                pygame.time.delay(cfg.SOLVER_DELAY)
         return False
 
     def mousepos_to_gridpos(self, mouse_pos):
@@ -123,10 +115,9 @@ class SudokuGrid():
         if self.squares[x][y].const:
             return False
 
-        if self.selected_sqr is not None:
-            self.selected_sqr.focus = False
+        self.deselect()
         self.selected_sqr = self.squares[x][y]
-        self.selected_sqr.focus = True
+        self.selected_sqr.highlighted = True
         return True
 
 
@@ -140,7 +131,7 @@ class SudokuGrid():
     def is_victorious(self):
         if not self.is_finished():
             return False
-        #check if everything is valid
+
         valid_rows = all([SudokuTools.is_valid(SudokuTools.get_row(self.matrix, i)) for i in range(9)])
         valid_cols = all([SudokuTools.is_valid(SudokuTools.get_col(self.matrix, i)) for i in range(9)])
         valid_boxes = all([SudokuTools.is_valid(SudokuTools.get_box(self.matrix, i, j)) for i in range(3) for j in range(3)])
@@ -154,14 +145,14 @@ class SudokuGrid():
         return True
 
 
-class Box():
-    def __init__(self, surf, row, col, val, focus = False):
+class Square():
+    def __init__(self, surf, row, col, val):
         self.row = row
         self.col = col 
         self.value = val
         self.const = val != 0 
         self.surface = surf
-        self.focus = focus
+        self.highlighted = False
 
         self.size = surf.get_width() / 9 
         self.x = col * self.size 
@@ -170,32 +161,32 @@ class Box():
 
     
     def draw(self):
-        font = pygame.font.SysFont(pygame.font.get_default_font(), 40)
-        text_color = GAME_DIGIT if self.const else PLAYER_DIGIT
+        font = pygame.font.SysFont(pygame.font.get_default_font(), cfg.DIGIT_FONT_SIZE)
+        text_color = cfg.DIGIT_CONST_COLOR if self.const else cfg.DIGIT_MUTABLE_COLOR
         text = font.render(str(self.value), 1, text_color)
         dest = (self.x + (self.size/2 - text.get_width()/2), self.y + (self.size/2 - text.get_height()/2))
         self.surface.blit(text, dest)
 
-        if self.focus:
-            pygame.draw.rect(self.surface, FOCUS_COLOR, self.rect, 3)
+        if self.highlighted:
+            pygame.draw.rect(self.surface, cfg.HIGHLIGHT_COLOR, self.rect, 3)
 
     def solver_draw(self, backtracked):
-        pygame.draw.rect(self.surface, WHITE, self.rect, 0)
+        pygame.draw.rect(self.surface, cfg.BG_COLOR, self.rect, 0)
         self.draw()
-        color = RED if backtracked else GREEN 
+        color = cfg.SOLVER_INP_BACKTRACK_COLOR if backtracked else cfg.SOLVER_INP_COLOR
         pygame.draw.rect(self.surface, color, self.rect, 3)
 
     def set(self, v):
         self.value = v
 
 pygame.init()
-window = pygame.display.set_mode(size=(800, 800))
-pygame.display.set_caption("SudokuSensei")
+window = pygame.display.set_mode(cfg.WINDOW_SIZE)
+pygame.display.set_caption(cfg.WINDOW_NAME)
 g = SudokuGrid(window, t)
 is_running = True
 
 while is_running:
-    window.fill(BG_COLOR)
+    window.fill(cfg.BG_COLOR)
     g.draw()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -208,16 +199,16 @@ while is_running:
             input = pygame.key.name(event.key)
             if input in "0123456789":
                 g.fill_selected_sqr(input)
-            if input == "return":
-                if g.is_victorious():
+            if input == cfg.KEY_CHECK_VICTORY:
+                if g.is_victorious():   #replace with some victory/losing screen
                     print("yay! :)")
                 else:
                     print("nay! :(")
-            if input == "space":
+            if input == cfg.KEY_RUN_SOLVER:
                 g.deselect()
                 g.reset_board()
                 g.solve_and_draw()
-            if input == "r":
+            if input == cfg.KEY_RESTART_BOARD:
                 g.reset_board()
 
     pygame.display.update()
---
+++ config.py
@@ -0,0 +1,30 @@
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+GRAY = (150, 150, 150)
+BLUE = (0, 0, 255)
+
+#window settings
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 600
+WINDOW_SIZE = (WINDOW_WIDTH, WINDOW_HEIGHT)
+WINDOW_NAME = "SudokuSensei"
+
+#board colors and sizes
+DIGIT_FONT_SIZE = 40
+BG_COLOR = WHITE            # board background color
+HIGHLIGHT_COLOR = BLUE      # color of square chosen by player
+DIGIT_CONST_COLOR = BLACK   # non-changeable digits
+DIGIT_MUTABLE_COLOR = GRAY  # digits changeable by player  
+LINE_COLOR = BLACK          # Sudoku board seperator
+
+#solver configs
+SOLVER_INP_COLOR = GREEN            # highlight color of filled squares
+SOLVER_INP_BACKTRACK_COLOR = RED    # highlight color of backtracked squares
+SOLVER_DELAY = 0            #defines delay in ms between solver inputs  
+
+#key bindings
+KEY_RUN_SOLVER = "space"
+KEY_RESTART_BOARD = "r"
+KEY_CHECK_VICTORY = "return"
